df45732ed28230668258cbccc009a421f47b4744
fg2406 <flzgltr11@gmail.com>
2022-02-04 09:38:25 +0100

    atm

--- eclipse-workspace/myProjects/src/projeler/Kitapci.java
+++ eclipse-workspace/myProjects/src/projeler/Kitapci.java
@@ -1,5 +1,479 @@
 package projeler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Kitapci {
 
-}
+    static int count=1000;
+	static List <String> kitapAdi=new ArrayList<>();
+	static List <String> yazarAdi=new ArrayList<>();
+	static List <Double> fiyati=new ArrayList<>();
+	static List <Integer> kitapNo=new ArrayList<>();
+	
+
+	public static void main(String[] args) {
+		
+		
+		/*  ====================PAZAR PROJEMIZ============================== 
+		 * bir kitapci icin program yazalim 
+		 * kitap no 1000'den baslayacak sirali no olsun
+		 * program baslayinca menu isminde bir method calissin 
+		 * 1-kitap ekle 
+		 * 2-numara ile kitap goruntule 
+		 * 3-bilgi ile kitap goruntule 
+		 * 4-numara ile kitap sil 
+		 * 5-tum kitaplar listele 
+		 * 6-cikis
+		 ************************************
+		 * 1.olarak her kitaba ait kitap no olacak, sonra kitap adi olacak, buna ait
+		   yazar adi olacak ve kitap fiyati olacak bu bilgilerin tutulacagi bir yer olmali yani bir list'te tutmamiz gerek 
+		   2-kitap numarasi 1000'den baslamali,her kitap eklendiginde bir artacagi icin sayac gibi dusunulebilinir
+		    count=1000; 
+		   menu isminde bir method olusturmaliyim,
+		   3-kitap ekle diye bir method olusturmliyim,bu method farkli bir classda
+		   4-kullanicidn kitap adi yazar adi ve fiyat bilgileri istenmeli
+		 */
+		
+		
+		System.out.println(        "        ********************SANAL KİTABEVİNE HOŞGELDİNİZ********************       "   );
+		
+		System.out.println("                 **********İŞLEMLERİNİZ ARTIK ÇOK KOLAY!************             ");
+		
+		System.out.println("    //// MENÜ ////                         ");
+		
+		
+		menu();
+		
+
+	}
+
+	@SuppressWarnings("resource")
+	private static void menu() {
+		
+		Scanner scan=new Scanner(System.in);
+		int secim=0;
+		
+		System.out.println("\nLütfen yapmak istediginiz işlemi seçiniz. Ardından enter tuşuna basınız.\n");
+			
+		System.out.println( "1-)KİTAP EKLE\n2-)NUMARA İLE KİTAP GÖRÜNTÜLE\n3-)KİTAP BİLGİSİ İLE KİTAP GÖRÜNTÜLE\n4-)NUMARA İLE KİTAP SİL\n5-)TÜM KİTAPLARI LİSTELE\n6-)ÇIKIŞ");
+		   
+		secim=scan.nextInt();
+		
+		switch (secim) {
+		case 1: 
+			System.out.println("\nKitap Ekleme Sekmesine Hoşgeldiniz.Lütfen Yönergeleri izleyiniz.");
+			kitapekle();
+		
+			break;
+
+		case 2:
+			
+			numarakitapgörüntüle();
+			
+			break;	
+			
+		case 3:
+			
+			bilgikitapgörüntüle();
+			
+			break;
+			
+		case 4:
+			
+			kitapsil();
+			
+			break;
+		
+		case 5:
+			
+			kitaplarilistele();
+			
+			break;
+		
+		case 6:
+			System.out.println("\n\t\t\t********************ÇIKIŞ İŞLEMİNİZ BAŞARI İLE GERÇEKLEŞTİRİLMİŞTİR. TEŞEKKÜR EDER, İYİ GÜNLER DİLERİZ********************");
+			
+			break;
+			
+		default:
+			
+			do {
+				System.out.println("YANLIŞ BİR GİRİŞ YAPTINIZ.LÜTFEN TEKRAR DENEYİNİZ.\n1-)KİTAP EKLE\n2-)NUMARA İLE KİTAP GÖRÜNTÜLE\n3-)KİTAP BİLGİSİ İLE KİTAP GÖRÜNTÜLE\n4-)NUMARA İLE KİTAP SİL\n5-)TÜM KİTAPLARI LİSTELE\n6-)ÇIKIŞ");
+			    secim=scan.nextInt();
+			} while (secim<1 || secim>6);
+			
+		}
+		
+	
+	}
+
+	private static void kitapekle() {
+		
+
+		
+		int karar=0;
+		String kitap="";
+		String yazar="";
+	    double ücret=0;
+		int toplamEklenen=0;
+	    
+		
+		do {
+		  
+			@SuppressWarnings("resource")
+			Scanner scan=new Scanner(System.in);
+			
+			System.out.println("Lütfen eklemek istediginiz kitabın adını giriniz : ");
+			kitap=scan.nextLine().toUpperCase();
+			
+			System.out.println("Lütfen kitabın yazarını giriniz : ");
+			yazar=scan.nextLine().toUpperCase();
+			
+			System.out.println("Lütfen kitabın fiyatını giriniz : ");
+			ücret=scan.nextDouble();
+			
+			
+			kitapAdi.add(kitap);
+			yazarAdi.add(yazar);
+			fiyati.add(ücret);
+		    kitapNo.add(count);
+			
+		    System.out.println("\nEklenen Kitap Bilgileri : "+"\n\nKitap No:"+kitapNo.get(toplamEklenen)  +"    "+"\nKitap Adı:"+kitap+"    "+"\nKitap Yazarı:"+yazar+"    "+"\nKitap Ücret:"+ücret+"$\n");
+		    
+		    count++;
+			
+			System.out.println("\nKitap eklemeye devam etmek için 1\nİşlemi sonlandırıp menüye dönmek için 2'yi tuşlayınız.\n");
+			karar=scan.nextInt();
+		
+			toplamEklenen++;
+			
+			
+		} while (karar==1);
+		
+		
+		
+		
+		  System.out.println("Kitap ekleme işlemi başarı ile gerçekleştirildi.Teşekkürler.\n");	
+		
+		  menu();
+
+	}	
+
+	@SuppressWarnings("resource")
+	private static void kitaplarilistele() {
+		
+		System.out.println("**********KİTAP LİSTESİ**************\n");
+		Scanner scan=new Scanner(System.in);
+	
+		if (kitapNo.size()>0) {
+			
+			System.out.println(" KİTAP NO        KİTAP ADI        KİTABIN YAZARI        KİTABIN FİYATI " );
+			for (int i = 0; i < kitapNo.size(); i++) {
+				
+				//System.out.println(" KİTAP NO        KİTAP ADI        KİTABIN YAZARI        KİTABIN FİYATI " );
+				System.out.println(kitapNo.get(i) + "        "  +  kitapAdi.get(i) + "        " + yazarAdi.get(i) + "         " + fiyati.get(i) );
+				
+			}
+				
+			System.out.println("\nMenüye dönmek için 1\nÇıkış yapmak için 2 tuşlayın.");
+			int karar=scan.nextInt();
+			
+			switch (karar) {
+			case 1:
+				menu();
+				break;
+			case 2:
+				System.out.println("\n********************ÇIKIŞ İŞLEMİNİZ BAŞARI İLE GERÇEKLEŞTİRİLMİŞTİR. TEŞEKKÜR EDER, İYİ GÜNLER DİLERİZ********************");
+				break;
+			default:
+				
+				do {
+					System.out.println("\nYANLIŞ BİR GİRİŞ YAPTINIZ.LÜTFEN TEKRAR DENEYİNİZ.\nMenüye dönmek için 1\\nÇıkış yapmak için 2 tuşlayın.\n");
+				    karar=scan.nextInt();
+				} while (karar<1 || karar>2);
+				
+				break;
+			}
+			
+		} 
+		
+		
+		if (kitapNo.size()<=0) {
+			System.out.println("\nHenüz eklenmiş bir kitap bulunmamaktadır. \n\n\n");
+			
+			System.out.println("\nMenüye dönmek için 1\nÇıkış yapmak için 2 tuşlayın.\n");
+			int karar2=scan.nextInt();
+			
+			switch (karar2) {
+			case 1:
+				menu();
+				break;
+			case 2:
+				System.out.println("\n********************ÇIKIŞ İŞLEMİNİZ BAŞARI İLE GERÇEKLEŞTİRİLMİŞTİR. TEŞEKKÜR EDER, İYİ GÜNLER DİLERİZ********************");
+				break;
+			default:
+				
+				do {
+					System.out.println("\nYANLIŞ BİR GİRİŞ YAPTINIZ.LÜTFEN TEKRAR DENEYİNİZ.\nMenüye dönmek için 1\\nÇıkış yapmak için 2 tuşlayın.\n");
+				    karar2=scan.nextInt();
+				} while (karar2<1 || karar2>2);
+				
+				break;
+			}
+			
+		}
+		
+		
+	
+		
+	}
+
+	private static void kitapsil() {
+		
+	@SuppressWarnings("resource")
+	Scanner scan=new Scanner(System.in);
+
+		int no=0;
+	    int karar=0;
+		@SuppressWarnings("unused")
+		int toplamSilinen=0;
+	    int index=0;
+		
+	    if (kitapNo.size()<=0) {
+			System.out.println("\nHenüz eklenmiş bir kitap bulunmamaktadır. \n\n\n");
+			
+			System.out.println("\nMenüye dönmek için 1\nÇıkış yapmak için 2 tuşlayın.\n");
+			int karar3=scan.nextInt();
+			
+			switch (karar3) {
+			case 1:
+				menu();
+				break;
+			case 2:
+				System.out.println("\n********************ÇIKIŞ İŞLEMİNİZ BAŞARI İLE GERÇEKLEŞTİRİLMİŞTİR. TEŞEKKÜR EDER, İYİ GÜNLER DİLERİZ********************");
+				break;
+			default:
+				
+				do {
+					System.out.println("\nYANLIŞ BİR GİRİŞ YAPTINIZ.LÜTFEN TEKRAR DENEYİNİZ.\nMenüye dönmek için 1\\nÇıkış yapmak için 2 tuşlayın.\n");
+				    karar3=scan.nextInt();
+				} while (karar3<1 || karar3>2);
+				
+				break;
+			}
+			
+		}
+		
+	    
+	    if (kitapNo.size()>0) {
+	    
+		do {
+		  
+		
+			
+			System.out.println("Lütfen silmek istediginiz kitabın nosunu giriniz : ");
+			no=scan.nextInt();
+			
+			index=kitapNo.indexOf(no);
+			
+			// System.out.println(index);
+			
+			 System.out.println("\nSilinen Kitap Bilgileri : "+"\n\nKitap No:"+kitapNo.get(index)  +"    "+"\nKitap Adı:"+kitapAdi.get(kitapNo.get(index))
+			  +"    "+"\nKitap Yazarı:"+yazarAdi.get(kitapNo.get(index))+"    "+"\nKitap Ücret:"+fiyati.get(kitapNo.get(index))+"\n");
+		
+		
+			kitapNo.remove(index);
+			yazarAdi.remove(index);
+			kitapAdi.remove(index);
+			fiyati.remove(index);
+
+			System.out.println("\nKitap silmeye devam etmek için 1\nİşlemi sonlandırmak için 2'yi tuşlayınız.\n");
+			karar=scan.nextInt();
+		
+			toplamSilinen++;
+			
+			
+		} while (karar==1);
+		
+		
+		
+		
+		  System.out.println("\nKitap silme işlemi başarı ile gerçekleştirildi. Teşekkürler. ");	
+		
+		  menu();
+	    }
+	
+		
+	}
+
+	private static void bilgikitapgörüntüle() {
+		
+		@SuppressWarnings("resource")
+		Scanner scan=new Scanner(System.in);
+		 
+		 String kitapismi="";
+         @SuppressWarnings("unused")
+		String yazarismi="";
+         
+         int karar5=0;
+         @SuppressWarnings("unused")
+		int toplamGörüntülenen=0;
+		 int index5=0;
+		
+		
+		if (kitapNo.size()<=0) {
+			System.out.println("\nHenüz eklenmiş bir kitap bulunmamaktadır. \n\n\n");
+			
+			System.out.println("\nMenüye dönmek için 1\nÇıkış yapmak için 2 tuşlayın.");
+			int karar4=scan.nextInt();
+			
+			switch (karar4) {
+			case 1:
+				menu();
+				break;
+			case 2:
+				System.out.println("\n********************ÇIKIŞ İŞLEMİNİZ BAŞARI İLE GERÇEKLEŞTİRİLMİŞTİR. TEŞEKKÜR EDER, İYİ GÜNLER DİLERİZ********************");
+				break;
+			default:
+				
+				do {
+					System.out.println("\nYANLIŞ BİR GİRİŞ YAPTINIZ.LÜTFEN TEKRAR DENEYİNİZ.\nMenüye dönmek için 1\\nÇıkış yapmak için 2 tuşlayın.\n");
+				    karar4=scan.nextInt();
+				} while (karar4<1 || karar4>2);
+				
+				break;
+			}
+			
+		}
+		
+		
+		 if (kitapNo.size()>0) {
+		
+		
+		
+
+				do {
+					  
+					
+					System.out.println("Lütfen görüntülemek istediginiz kitabın adını giriniz : ");
+					
+					kitapismi=scan.nextLine().toUpperCase();
+					
+					index5=kitapAdi.indexOf(kitapismi);
+					
+					
+					System.out.println(index5);
+					
+				System.out.println("\nGörüntülenen Kitap Bilgileri: "+"\n\nKitap No: "+kitapNo.get(index5)  +"    "+"\nKitap Adı: "+kitapAdi.get(index5)
+				  +"    "+"\nKitap Yazarı: "+yazarAdi.get(index5)+"    "+"\nKitap Ücret: "+fiyati.get(index5)+"\n");
+				
+		
+
+					System.out.println("\nBaşka kitap görüntülemek için 1\nİşlemi sonlandırıp menüye dönmek için 2'yi tuşlayınız. ");
+					karar5=scan.nextInt();
+				
+					toplamGörüntülenen++;
+					
+					
+				} while (karar5==1);
+				
+				
+				
+				
+				  System.out.println("\nKitap görüntüleme işlemi başarı ile gerçekleştirildi. Menüye yönlendiriliyorsunuz.\n ");	
+				
+				  menu();
+		
+		
+		
+		
+		 }
+	}
+
+	private static void numarakitapgörüntüle() {
+		
+          @SuppressWarnings("resource")
+		Scanner scan=new Scanner(System.in);
+		
+          int index2=0;
+          int no2=0;
+          int karar7=0;
+          @SuppressWarnings("unused")
+		int toplamGörüntülenen=0;
+		
+		
+		if (kitapNo.size()<=0) {
+			System.out.println("Henüz eklenmiş bir kitap bulunmamaktadır. \n\n\n");
+			
+			System.out.println("\nMenüye dönmek için 1\nÇıkış yapmak için 2 tuşlayın.");
+			int karar6=scan.nextInt();
+			
+			switch (karar6) {
+			case 1:
+				menu();
+				break;
+			case 2:
+				System.out.println("\n********************ÇIKIŞ İŞLEMİNİZ BAŞARI İLE GERÇEKLEŞTİRİLMİŞTİR. TEŞEKKÜR EDER, İYİ GÜNLER DİLERİZ********************");
+				break;
+			default:
+				
+				do {
+					System.out.println("\nYANLIŞ BİR GİRİŞ YAPTINIZ.LÜTFEN TEKRAR DENEYİNİZ.\nMenüye dönmek için 1\\nÇıkış yapmak için 2 tuşlayın.\n");
+				    karar6=scan.nextInt();
+				} while (karar6<1 || karar6>2);
+				
+				break;
+			}
+			
+		}
+		
+		
+		
+		 if (kitapNo.size()>0) {
+		
+		
+		
+				do {
+					  
+					
+					
+					System.out.println("Lütfen görüntülemek istediginiz kitabın nosunu giriniz : ");
+					no2=scan.nextInt();
+					
+					index2=kitapNo.indexOf(no2);
+					
+					// System.out.println(index2);
+					
+					 System.out.println("\nGörüntülenen Kitap Bilgileri: "+"\n\nKitap No: "+kitapNo.get(index2)  +"    "+"\nKitap Adı: "+kitapAdi.get(index2)
+					  +"    "+"\nKitap Yazarı: "+yazarAdi.get(index2)+"    "+"\nKitap Ücret: "+fiyati.get(index2)+"\n");
+				
+		
+
+					System.out.println("\nBaşka kitap görüntülemek için 1\nİşlemi sonlandırıp menüye dönmek için 2'yi tuşlayınız. ");
+					karar7=scan.nextInt();
+				
+					toplamGörüntülenen++;
+					
+					
+				} while (karar7==1);
+				
+				
+				
+				
+				  System.out.println("\nKitap görüntüleme işlemi başarı ile gerçekleştirildi. Teşekkürler.\n ");	
+				
+				  menu();
+		
+	
+		
+		 }
+		
+	}
+
+
+		
+		
+		
+	}
--- eclipse-workspace/myProjects/src/projeler/Otomat.java
+++ eclipse-workspace/myProjects/src/projeler/Otomat.java
@@ -1,5 +1,156 @@
 package projeler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Otomat {
 
-}
+	/* projemizin adı YIYECEK OTOMATI
+	--15 adet ürün ve bu ürünlerin fiyatlarını belirleyiniz. *****
+	Bu ürünler ekranda listelensin****************
+	--Ürün seçip, para miktarı giriniz*******************************
+	--girdiğiniz para fazla ise üstünü versin, ***********
+	eksik ise ekleme yapmanızı istesin******************
+	--Ek ürün seçme seçeneği olsun, siz devam ettiğiniz sürece, **********************
+	para kontrolünü de yaparak işlem yapmaya devam etsin...************************************
+	NOT: Otomat sadece 1 tl , 5 tl, 10 tl, 20 tl miktarlarını kabul etsin !(paraMiktari==1 || parmiktari==5 || paramiktari==10 || paraMiktari==20)
+	oop concepte ait öğrendiklerimizi kullanmaya çalışalım...*************************
+	iyi çalışmalar
+	Kolay gelsin...
+	1- ne istedigini sor*****
+	2- fiyatini hesabla******
+	3- parasini al**********
+	4- cikis   */
+	
+	static List <String> urun= new ArrayList<>(Arrays.asList( "Albeni","Halley","Çizi","Cips","Bisküvi","Gofret","Biskrem","Sakız","Redbull","Cola","Fanta","Ayran","Su","Sprite","Soda" ));
+    static List <Double> urunFiyat=new ArrayList<>(Arrays.asList(5.0 , 5.0 , 2.0 , 5.0 , 4.0 , 3.5 , 3.0 , 1.0 , 5.0 , 3.5 , 4.0 , 3.0 , 2.0 , 1.5 , 1.5 ));
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+	
+	Scanner scan=new Scanner(System.in);	
+	
+	System.out.println("    **********HOŞGELDİNİZ**********    \n");	
+	System.out.println("  **********KOLAY ve PRATİK********** \n\n");
+		
+	System.out.println(" *****OTOMAT ÜRÜNLER***** \n");	
+		
+	System.out.println("NO\tÜRÜN LİSTESİ \tÜRÜN FİYATI");	
+		
+	
+	for (int i = 0; i < urun.size(); i++) {
+		System.out.println(" " + (i+1) + "\t" + urun.get(i) + "\t" + "\t"  +  urunFiyat.get(i) );
+	}
+	
+	int urunKodu=0;
+	double urunMiktari=0;
+	int secim=0;
+	double paraMiktari=0;
+	double ekTutar=0;
+	
+	
+	
+	do {
+	
+	System.out.println("\nLütfen almak istediginiz ürün kodunu giriniz: ");
+	urunKodu=scan.nextInt();   // 1den 15e kadar girecek
+	
+	if (urunKodu>15 || urunKodu<=0) {
+	       
+		System.out.println("Geçersiz giriş yaptınız.Lütfen tekrar deneyiniz.");
+		
+		
+	} else {
+		
+		System.out.println("Seçilen ürün: "  + urun.get(urunKodu-1)   );
+		System.out.println("Ürünün fiyatı: "  + urunFiyat.get(urunKodu-1)   );
+		
+		urunMiktari+=urunFiyat.get(urunKodu-1);	
+		
+	}
+	
+	System.out.println("\nÜrün eklemek için herhangi bir tuşa basınız, kasaya gitmek için 2'yi tuşlayınız.");
+	secim=scan.nextInt();
+	
+	} while (!(secim==2));
+	
+	
+	System.out.println("\nToplam ödenmesi gereken tutar: " + urunMiktari );
+	
+	
+	
+	
+	do {
+	
+	
+	System.out.println("Lütfen ödemek istediginiz miktarı giriniz:\nLütfen sadece 1$,5$,10$,20$ banknotlardan giriniz.");
+	paraMiktari=scan.nextDouble();
+	
+	
+	if (!(paraMiktari==1 || paraMiktari==5 || paraMiktari==10 || paraMiktari==20) ) {
+	  
+		System.out.println("Geçersiz miktar girişi yapmaya çalıştınız.Otomata sadece 1$, 5$, 10$, 20$ banknotlardan yatırabilirsiniz."+
+		                                                  "Lütfen tekrar deneyiniz."); 
+		
+	}
+	
+	
+	} while ( !(paraMiktari==1 || paraMiktari==5 || paraMiktari==10 || paraMiktari==20) ) ;
+	
+	/////////////////////
+	
+	
+	
+	if (paraMiktari>=urunMiktari) {
+		
+		System.out.println("Girişi yapılan miktar: " + paraMiktari + "\nPara üstünüz: " + (paraMiktari-urunMiktari) + "$'dır"
+		+   "\n*****Teşekkür eder, iyi günler dileriz*****"  );
+		
+		
+		
+	} else if (paraMiktari<urunMiktari) {
+
+		
+	   do {
+		
+		System.out.println("Eksik tutar girdiniz. Talep edilen ürün fiyatı: " + urunMiktari + "\nGirilen miktar: " +paraMiktari 
+				+ "\nÖdenmesi gereken ek tutar: " +(urunMiktari-paraMiktari) + "Lütfen belirtilen tutarı giriniz: "  ) ;
+		
+		ekTutar=scan.nextDouble(); //3
+	
+		
+		// kullanıcı istenilen paralardan girmeyebilir
+		if (  !(ekTutar==1 || ekTutar==5 || ekTutar==10 || ekTutar==20)  ) {
+			System.out.println("İşlem başarısız. Geçersiz miktar girişi yaptınız." + "Otomata sadece 1$, 5$, 10$, 20$ banknotlardan yatırabilirsiniz."+
+                    "Lütfen tekrar deneyiniz.");
+		
+			
+		// kullanıcı parayı istenilen banknolardan girip istenilen fiyatın ödemesini yapabilir.
+		} else {
+			paraMiktari+=ekTutar;
+		}
+		
+		if (paraMiktari>=urunMiktari) {
+
+			System.out.println("Girişi yapılan miktar: " + paraMiktari + "\nPara üstünüz: " + (paraMiktari-urunMiktari) + "$'dır"
+					+   "\n*****Teşekkür eder, iyi günler dileriz*****"  );
+	   // break;
+			urunMiktari=paraMiktari;
+				// kullanıcı ek tutarı giriken sonra yine de istenilen ücrete ulaşamayabilir.	
+		}  else {
+			
+			System.out.println("Eksik tutar girdiniz.Lütfen tekrar istenilen tutarı giriniz: ");
+			}
+		
+		} while ( !(urunMiktari==paraMiktari) );
+		
+		
+		
+	   }
+	   
+	}}
+
+	
+
